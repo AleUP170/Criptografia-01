97f68a85b2d4af1b42fe61aea18c7525031462b8
Alejandro <alejandroup170@gmail.com>
2020-10-12 13:46:06 -0500

    Update program.py

--- program.py
+++ program.py
@@ -5,8 +5,9 @@ lines = []
 for line in fileinput.input():
     lines.append(line)
 
-#Obtener las llaves y texto plano
-#key = lines[1]
-#text = lines[2].replace(" ","") 
+#Obtener las llaves y texto plano, limpiando el input
+key = lines[0].replace("\n","")
+text = lines[1].replace(" ","") 
+text = text.replace("\n","") 
 
-print(lines)
+print(key, text)
