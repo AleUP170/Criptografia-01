b7d99d992c60d1fe6c7d5eab2b94db858bd27487
Alejandro <alejandroup170@gmail.com>
2020-10-12 14:07:47 -0500

    Update program.py

--- program.py
+++ program.py
@@ -3,11 +3,12 @@ import fileinput
 # Funcion que crea al cuadro 
 def crearCuadro(alph, key):
     cuadro = {}
-    for i in range (0,len(key)):
-        cuadro[key[i]] = i
-        alph.replace(key[i],"")
-    for i in range (len(key), 26):
-        cuadro[alph[i]] = i    
+    for i in range (0,5):
+        for j in range (0,5):
+            if i + j < len(key):
+                cuadro[key[i+j]]="{}{}".format(i,j)
+            elif alph[i+j] not in cuadro:
+                cuadro[alph[i+j]]="{}{}".format(i,j)
     return cuadro
 
 # Lee entradas
@@ -15,7 +16,7 @@ lines = []
 for line in fileinput.input():
     lines.append(line)
 
-alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ"
 
 #Obtener las llaves y texto plano, limpiando el input
 key = lines[0].replace("\n","")
